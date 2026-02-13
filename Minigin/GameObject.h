6a34c6e8cc8d25148f3091abe5ba3781caef2e3f
wstoop <warre.stoop@gmail.com>
2026-02-13 23:59:09 +0100

    Cleanup

--- Minigin/GameObject.h
+++ Minigin/GameObject.h
@@ -51,7 +51,7 @@ namespace dae
 		}
 
 		GameObject() = default;
-		virtual ~GameObject();
+		~GameObject();
 		GameObject(const GameObject& other) = delete;
 		GameObject(GameObject&& other) = delete;
 		GameObject& operator=(const GameObject& other) = delete;
